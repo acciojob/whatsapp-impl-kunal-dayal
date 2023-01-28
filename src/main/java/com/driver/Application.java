a2119d7fd152c2bc88da7906ea988a9d1859507b
kunal <kunaldayal.786.kd@gmail.com>
2023-01-28 20:01:26 +0530

    lets see what happens

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -3,6 +3,9 @@ package com.driver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//hardcode
+
 @SpringBootApplication
 public class Application {
 
